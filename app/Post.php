46902ebb370bb65242cc7bdad9baff1203fcd40b
danvop <danvop@gmail.com>
2017-02-23 17:22:17 +1000

    add commets form made

--- app/Post.php
+++ app/Post.php
@@ -2,10 +2,20 @@
 
 namespace App;
 
+use App\Comment;
+
 class Post extends Model
 {
     public function comments()
     {
         return $this->hasMany(Comment::class);
+    }
+    public function addComment($body)
+    {
+        $this->comments()->create(compact('body'));
+        // Comment::create([
+        //     'body' => $body,
+        //     'post_id' => $this->id
+        //     ]);
     }
 }
